c1cbedbca31695ad632ddf61366efc366ad8b516
rch850 <rich850@gmail.com>
2012-12-16 10:43:04 +0900

    Support all ec2 endpoints.

--- lib/monogusa/app.rb
+++ lib/monogusa/app.rb
@@ -15,12 +15,28 @@ module Monogusa
     enable :sessions
     set :session_secret, "3dA6r82zFi6HAIbCfBGMZCV88vsxJHM1"
 
+    # List of regions http://docs.amazonwebservices.com/general/latest/gr/rande.html
+    EC2_ENDPOINTS = %w(
+      ec2.us-east-1.amazonaws.com
+      ec2.us-west-2.amazonaws.com
+      ec2.us-west-1.amazonaws.com
+      ec2.eu-west-1.amazonaws.com
+      ec2.ap-southeast-1.amazonaws.com
+      ec2.ap-southeast-2.amazonaws.com
+      ec2.ap-northeast-1.amazonaws.com
+      ec2.sa-east-1.amazonaws.com
+    )
+
     get "/" do
       if session[:access_key_id]
-        @ec2 = AWS::EC2.new(:access_key_id => session[:access_key_id],
-                           :secret_access_key => session[:secret_access_key])
+        @ec2 = Hash.new
+        EC2_ENDPOINTS.each do |ep|
+          @ec2[ep] = AWS::EC2.new(:access_key_id => session[:access_key_id],
+                                  :secret_access_key => session[:secret_access_key],
+                                  :ec2_endpoint => ep)
+        end
         @s3 = AWS::S3.new(:access_key_id => session[:access_key_id],
-                         :secret_access_key => session[:secret_access_key])
+                          :secret_access_key => session[:secret_access_key])
         slim :index
       else
         redirect to("/login")
